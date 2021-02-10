d20bf51b79fc45b810a674ebac4088ca9eb1683c
klylylydeee <klylylydeee@gmail.com>
2021-02-10 23:51:42 +0800

    test ember routing system and add notes

--- app/router.js
+++ app/router.js
@@ -6,4 +6,10 @@ export default class Router extends EmberRouter {
   rootURL = config.rootURL;
 }
 
-Router.map(function () {});
+Router.map(function () {
+  this.route('test', function() {
+    this.route('nested');
+  });
+  this.route('item', { path: '/item/:item_id'});
+  this.route('not-found', { path: '/*path'});
+});
